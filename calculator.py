0a5d58351f6b4c5d2a9def2ba00725158fd24911
mdracc <adinair47@gmail.com>
2026-04-08 12:46:43 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,25 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+# https://github.com/zmckirdy/lab11-ZM-AN.git
+# Partner 1: Zach McKirdy
+# Partner 2: Aditya Nair
 
-One function per operation, in order.
-"""
-# First example
+import math
+
 def add(a, b): 
-    pass
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b): 
+    return a * b
+
+def div(a, b): 
+    return b / a 
+
+def log(a, b): 
+    return math.log(b,a)
+
+def exp(a, b): 
+    return a^b
 
 
